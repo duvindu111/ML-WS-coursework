0e24df3d48b12d588f18a956a9dbcd951b4ffa07
duvindu111 <duvinduthathsara@gmail.com>
2025-04-05 11:40:18 +0530

    files required for deployment added

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=80)
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=80)
+    # app.run(debug=True, port=5001)
